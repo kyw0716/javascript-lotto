cbb1b702f07eaf390da37fd855fcad8453279ad5
kyw0716 <kyw0716@naver.com>
2022-11-29 23:05:11 +0900

    feat: 구매한 로또를 보여주는 기능

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -2,6 +2,7 @@ const Lotto = require("../Lotto");
 const { PRICE_FOR_ONE_LOTTO } = require("../static/Static");
 const { LottoNumberGenerator } = require("../utils/LottoNumberGenerator");
 const { InputView } = require("../view/InputView");
+const { OutputView } = require("../view/OutputView");
 
 class LottoGameController {
   #lottos = [];
@@ -12,9 +13,21 @@ class LottoGameController {
     InputView.readPurchasePrice((input) => {
       const purchaseAmount = input / PRICE_FOR_ONE_LOTTO;
 
+      OutputView.printPurchaseAmount(purchaseAmount);
+
       for (let i = 0; i < purchaseAmount; i++) {
         this.#lottos.push(new Lotto(LottoNumberGenerator.generate()));
       }
+
+      this.displayPurchasedLottos();
     });
   }
+
+  displayPurchasedLottos() {
+    OutputView.printLottos(this.#lottos);
+  }
 }
+
+module.exports = { LottoGameController };
+
+new LottoGameController().inputPurchasePrice();
---
+++ src/view/OutputView.js
@@ -0,0 +1,15 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printPurchaseAmount(amount) {
+    Console.print(`\n${amount}개를 구매했습니다.`);
+  },
+
+  printLottos(lottos) {
+    for (let i = 0; i < lottos.length; i++) {
+      Console.print(lottos[i].getLottoNumber());
+    }
+  },
+};
+
+module.exports = { OutputView };
